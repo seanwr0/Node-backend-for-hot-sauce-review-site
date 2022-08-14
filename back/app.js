77d644247b0d17d4b558ee6efb394427c2748b52
sean <srosen6136@mpc.edu>
2022-08-14 09:44:24 -0700

    added signup and sign-in

--- back/app.js
+++ back/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+const app = express();
 
+app.use('/api/auth', userRoutes);
 mongoose.connect('mongodb+srv://sean:secretpassword@cluster0.sc0ja.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
     console.log('Successfully connected to MongoDB Atlas!');
@@ -10,11 +13,4 @@ mongoose.connect('mongodb+srv://sean:secretpassword@cluster0.sc0ja.mongodb.net/?
     console.error(error);
   });
 
-
-const app = express();
-
-app.use((req, res) => {
-   res.json({ message: 'Your request was successful!' }); 
-});
-
-module.exports = app
+module.exports = app;
---
+++ back/controllers/users.js
@@ -0,0 +1,62 @@
+const users = require('../models/users');
+const bcrypt= require('bcrypt');
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(
+    (hash) => {
+      const user = new users({
+        email: req.body.email,
+        password: hash,
+      });
+      user.save().then(
+        () => {
+          res.status(201).json({
+            message: 'user saved to database!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    })
+};
+
+exports.checkUser = (req, res, next) => {
+  users.findOne({ email: req.body.email }).then(
+    (user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found!')
+        });
+      }
+      bcrypt.compare(req.body.password, user.password).then(
+        (valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password!')
+            });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'token'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    }
+  ).catch(
+    (error) => {
+      res.status(500).json({
+        error: error
+      });
+    }
+  );
+}
