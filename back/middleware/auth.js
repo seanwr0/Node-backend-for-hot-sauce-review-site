def907faf777d72360b9b30f9fb546c6e43bb395
sean <srosen6136@mpc.edu>
2022-08-20 20:06:08 -0700

    created sauces model

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -11,8 +11,8 @@ module.exports = (req, res, next) => {
       next();
     }
   } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
+    res.status(400).json({
+      error: new Error('bad request!')
     });
   }
 };
